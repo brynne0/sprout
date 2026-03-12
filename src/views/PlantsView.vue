712afe14288d1ed7d39f78c247522ed2e07bf9a3
brynne0 <brynne.obrien@me.com>
2026-03-12 16:21:17 +0100

    Add shadcn table component and PlantsView.

---
+++ src/views/PlantsView.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+</script>
+
+<template>
+  <main>
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead class="w-25">Plant Name</TableHead>
+          <TableHead>Sowing Date</TableHead>
+          <TableHead>Transplant Date</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <TableRow>
+          <TableCell class="font-medium">Tomato</TableCell>
+          <TableCell>01.03.26</TableCell>
+          <TableCell>04.03.26</TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  </main>
+</template>
