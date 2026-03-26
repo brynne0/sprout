44baa18757f48397ce727ceeb381f7f7ee63aaf9
brynne0 <brynne.obrien@me.com>
2026-03-26 21:42:04 +0100

    Add V3 db migration for all crops to be filled in with description and harvest notes.

---
+++ sprout-api/db/migrations/V3_catalogue_descriptions.sql
@@ -0,0 +1,221 @@
+UPDATE plant_catalogue SET description = '', harevest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Broad Beans') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Climbing Beans') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Dwarf French Beans') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Peas') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Broccoli') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Broccoli') AND variety = 'Calabrese';
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Broccoli') AND variety = 'Purple Sprouting';
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Broccoli') AND variety = 'Kaibroc';
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Brussels Sprouts') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Cabbage') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Cabbage') AND variety = 'Spring';
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Cabbage') AND variety = 'Summer';
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Cabbage') AND variety = 'Autumn';
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Cabbage') AND variety = 'Winter';
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Cauliflower') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Kale') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Radish') AND variety = 'Spring';
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Radish') AND variety = 'Winter';
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Swede') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Turnip') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Garlic') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Leek') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Spring Onion') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Bulb Onion and Shallot') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Overwintering Onion') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Zucchini') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Cucumber') AND variety = 'Ridge';
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Cucumber') AND variety = 'Cordon';
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Melon') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Pumpkin') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Carrot') AND variety = 'Early';
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Carrot') AND variety = 'Maincrop';
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Celeriac') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Celery') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Bulb Fennel') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Parsnip') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Coriander') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Chervil') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Dill') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Parsley') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Beetroot') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Chard') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Spinach') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Chicory') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Endive') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Lettuce') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Wombok') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Mizuna and Mustards') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Pak Choi') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Rocket') AND variety = 'Salad';
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Rocket') AND variety = 'Wild';
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Corn Salad') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Land Cress') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Winter Purslane') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Eggplant') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Capsicum') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Chilli') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Basil') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Potato') AND variety = 'First Early';
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Potato') AND variety = 'Second Early';
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Potato') AND variety = 'Maincrop';
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Sweetcorn') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Tomato') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Asparagus') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Perennial Kale') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Rhubarb') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Sorrel') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Chives') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Mint') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Rosemary') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Thyme') AND variety IS NULL;
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Oregano') AND variety = 'Hera';
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Artichoke') AND variety = 'Green Globe';
+
+UPDATE plant_catalogue SET description = '', harvest = ''
+WHERE plant_type_id = (SELECT id FROM plant_types WHERE name = 'Wild Strawberry') AND variety = 'Baron Solemacher';
